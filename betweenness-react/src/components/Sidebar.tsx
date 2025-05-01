deeb2b6e28078756deb0f7ee6ea5c9081dd20ce8
Amirhossein Kiani <amirhkiani98@yahoo.com>
2025-04-30 21:50:46 -0500

    Add framer-motion and motion dependencies; enhance Sidebar component with motion effects

--- betweenness-react/src/components/Sidebar.tsx
+++ betweenness-react/src/components/Sidebar.tsx
@@ -1,23 +1,31 @@
+import { motion } from "motion/react";
+import "../App.css";
 export function Sidebar() {
     return (
         <div className="p-4 bg-gray-200 h-full w-80 overflow-y-auto absolute right-0 z-50">
             <h2 className="text-xl font-bold mb-4">Graph Controls</h2>
 
-            <button className="bg-blue-500 text-white py-2 px-4 rounded w-full mb-2 hover:bg-blue-600">Find Best Betweenness</button>
-            <button className="bg-blue-500 text-white py-2 px-4 rounded w-full mb-2 hover:bg-blue-600">Reset Graph</button>
+            <button>Find Best Betweenness</button>
+            <button >Reset Graph</button>
 
             <div className="mt-4">
-            <input type="text" placeholder="Node Name" className="border border-gray-300 rounded py-2 px-3 w-full mb-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
-            <button className="bg-green-500 text-white py-2 px-4 rounded w-full mb-2 hover:bg-green-600">Add Node</button>
+            <input type="text" placeholder="Node Name" />
+            <motion.input
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+                transition={{ duration: 1 }}
+                type="text"
+                placeholder="Motion working!"
+                />
             </div>
 
             <div className="mt-4">
-            <select defaultValue="" id="from-selection" className="border border-gray-300 rounded py-2 px-3 w-full mb-2 focus:outline-none focus:ring-2 focus:ring-blue-500">
+            <select defaultValue="" id="from-selection">
                 <option value="" disabled>From</option>
                 <option value="node1">Node 1</option>
                 <option value="node2">Node 2</option>
             </select>
-            <select defaultValue="" id="to-selection" className="border border-gray-300 rounded py-2 px-3 w-full mb-2 focus:outline-none focus:ring-2 focus:ring-blue-500">
+            <select defaultValue="" id="to-selection">
                 <option value="" disabled>To</option>
                 <option value="node1">Node 1</option>
                 <option value="node2">Node 2</option>
